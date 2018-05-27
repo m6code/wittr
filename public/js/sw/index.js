14252759932d2c9e7b313cdfca9f6ee5f12570bb
Benjamin M6 <benjamynfrank101@gmail.com>
2018-05-27 12:22:57 +0100

    complete task serve-skeleton

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,4 +1,4 @@
-var staticCacheName = 'wittr-static-v3'; // name of current cache
+var staticCacheName = 'wittr-static-v4'; // name of current cache
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
@@ -7,7 +7,7 @@ self.addEventListener('install', function(event) {
     // TODO: cache /skeleton rather than the root page
 
       return cache.addAll([
-        '/',
+        '/skeleton',
         'js/main.js',
         'css/main.css',
         'imgs/icon.png',
@@ -37,6 +37,14 @@ self.addEventListener('activate', function(event) {
 self.addEventListener('fetch', function(event) {
   // TODO: respond to requests for the root page with
   // the page skeleton from the cache
+  var requestUrl = new URL(event.request.url);
+
+  if(requestUrl.origin === location.origin){
+    if(requestUrl.pathname === '/'){
+      event.respondWith(caches.match('/skeleton'));
+      return;
+    }
+  }
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
