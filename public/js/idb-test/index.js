ab25e42ee193d90ba8e1f6bc046ee1863a792e9d
Benjamin M6 <benjamynfrank101@gmail.com>
2018-05-31 08:43:38 +0100

    Checkout Idb-cusoring

--- public/js/idb-test/index.js
+++ public/js/idb-test/index.js
@@ -11,10 +11,9 @@ var dbPromise = idb.open('test-db', 4, function(upgradeDb) {
       var peopleStore = upgradeDb.transaction.objectStore('people');
       peopleStore.createIndex('animal', 'favoriteAnimal');
     case 3:
-      var peopleStore = upgradeDb.transaction.objectStore('people');
+      peopleStore = upgradeDb.transaction.objectStore('people');
       peopleStore.createIndex('age', 'age');
   }
-  // TODO: create an index on 'people' named 'age', ordered by 'age'
 });
 
 // read "hello" in "keyval"
@@ -90,13 +89,34 @@ dbPromise.then(function(db) {
   console.log('Cat people:', people);
 });
 
-// TODO: console.log all people ordered by age
-dbPromise.then(function(db){
+// people by age
+dbPromise.then(function(db) {
   var tx = db.transaction('people');
   var peopleStore = tx.objectStore('people');
   var ageIndex = peopleStore.index('age');
 
   return ageIndex.getAll();
-}).then(function(people){
-  console.log('Age: ', people);
+}).then(function(people) {
+  console.log('People by age:', people);
+});
+
+// Using cursors
+dbPromise.then(function(db) {
+  var tx = db.transaction('people');
+  var peopleStore = tx.objectStore('people');
+  var ageIndex = peopleStore.index('age');
+
+  return ageIndex.openCursor();
+}).then(function(cursor) {
+  if (!cursor) return;
+  return cursor.advance(2);
+}).then(function logPerson(cursor) {
+  if (!cursor) return;
+  console.log("Cursored at:", cursor.value.name);
+  // I could also do things like:
+  // cursor.update(newValue) to change the value, or
+  // cursor.delete() to delete this entry
+  return cursor.continue().then(logPerson);
+}).then(function() {
+  console.log('Done cursoring');
 });
