4542aa6b7ac990b38529276534fe1cd52f45b603
Benjamin M6 <benjamynfrank101@gmail.com>
2018-06-01 11:22:08 +0100

    Stub for creating wittr database

--- public/js/main/IndexController.js
+++ public/js/main/IndexController.js
@@ -2,12 +2,27 @@ import PostsView from './views/Posts';
 import ToastsView from './views/Toasts';
 import idb from 'idb';
 
+function openDatabase() {
+  // If the browser doesn't support service worker,
+  // we don't care about having a database
+  if (!navigator.serviceWorker) {
+    return Promise.resolve();
+  }
+
+  // TODO: return a promise for a database called 'wittr'
+  // that contains one objectStore: 'wittrs'
+  // that uses 'id' as its key
+  // and has an index called 'by-date', which is sorted
+  // by the 'time' property
+}
+
 export default function IndexController(container) {
   this._container = container;
   this._postsView = new PostsView(this._container);
   this._toastsView = new ToastsView(this._container);
   this._lostConnectionToast = null;
   this._openSocket();
+  this._dbPromise = openDatabase();
   this._registerServiceWorker();
 }
 
@@ -36,11 +51,14 @@ IndexController.prototype._registerServiceWorker = function() {
     });
   });
 
-  // TODO: listen for the controlling service worker changing
-  // and reload the page
-  navigator.serviceWorker.addEventListener('controllerchange', function(){
+  // Ensure refresh is only called once.
+  // This works around a bug in "force update on reload".
+  var refreshing;
+  navigator.serviceWorker.addEventListener('controllerchange', function() {
+    if (refreshing) return;
     window.location.reload();
-  })
+    refreshing = true;
+  });
 };
 
 IndexController.prototype._trackInstalling = function(worker) {
@@ -59,7 +77,6 @@ IndexController.prototype._updateReady = function(worker) {
 
   toast.answer.then(function(answer) {
     if (answer != 'refresh') return;
-    // TODO: tell the service worker to skipWaiting
     worker.postMessage({action: 'skipWaiting'});
   });
 };
@@ -112,5 +129,13 @@ IndexController.prototype._openSocket = function() {
 // called when the web socket sends message data
 IndexController.prototype._onSocketMessage = function(data) {
   var messages = JSON.parse(data);
+
+  this._dbPromise.then(function(db) {
+    if (!db) return;
+
+    // TODO: put each message into the 'wittrs'
+    // object store.
+  });
+
   this._postsView.addPosts(messages);
 };
